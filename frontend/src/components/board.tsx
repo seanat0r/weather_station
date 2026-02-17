fd9a5d8b72e20f69e6e0d39ab563ad5d8f8325ed
seanatorhd@gmail.com <deine-email@beispiel.de>
2026-02-17 16:10:09 +0100

    feat: added dynamic weather graph

--- frontend/src/components/board.tsx
+++ frontend/src/components/board.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import Graph from "./graph"
 import { type WeatherHistory, getWeatherHistory } from './../services/weatherService'
 import { type GraphData, icons } from "./../config/config"
@@ -6,42 +6,50 @@ import { type GraphData, icons } from "./../config/config"
 function Tile() {
   const [dailyData, setDailyData] = useState<WeatherHistory | null>(null);
   const [weeklyData, setWeeklyData] = useState<WeatherHistory | null>(null);
-  const [monthlyData, setMonthlyData] = useState<WeatherHistory | null>(null); 
-  const task = [{time: '24', setter: 'setDailyData'}, {time: '1W', setter: 'setWeeklyData'}, {time: '1M', setter: 'setMonthlyData'}];
+  const [monthlyData, setMonthlyData] = useState<WeatherHistory | null>(null);
+  const task = [{ time: '24', setter: setDailyData, state: dailyData },
+  { time: '1W', setter: setWeeklyData, state: weeklyData },
+  { time: '1M', setter: setMonthlyData, state: monthlyData }
+  ];
+  const METRICS = [
+    { title: 'temperature', unit: 'ºC', keys: ['indoor_temp', 'outdoor_temp'] },
+    { title: 'humidity', unit: '%', keys: ['indoor_hum', 'outdoor_hum'] },
+    { title: 'pressure', unit: 'hPa', keys: ['indoor_press', 'outdoor_press'] },
+    { title: 'rain', unit: 'mm', keys: ['rain'] },
+    { tilte: 'windspeed', unit: 'km/h', keys: ['wind_speed'] }
+  ];
 
-  useEffect ((time:string, setter) => {
-    const fetchWeather = async () => {
-      const data = await getWeatherHistory(time)
-      setter(data);
+  useEffect(() => {
+    const fetchAllWeather = async () => {
+      task.forEach(async (item) => {
+        const data = await getWeatherHistory(item.time);
+        item.setter(data);
+      })
     }
-    fetchWeather(time, setter);
-  })
+    fetchAllWeather()
+  }, [])
 
-  function mappingApiCalls() {
 
-  }
-  
-  }
-    return (
-        <>
-            <div className="tile">
-              {/* Graphen durch mappen */}
-                <Graph /> {/* 24 h temp */}
-                <Graph /> {/* 07 d temp */}
-                <Graph /> {/* 01 M temp */}
-                <Graph /> {/* 24 h hum */}
-                <Graph /> {/* 07 d hum */}
-                <Graph /> {/* 01 M hum */}
-                <Graph /> {/* 24 h press */}
-                <Graph /> {/* 07 d press*/}
-                <Graph /> {/* 01 M press*/}
-                <MetricBox />
-                <MetricBox />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="tile">
+        {/* Graphen durch mappen */}
+        <Graph data={dailyData} metric={METRICS[0]} /> {/* 24 h temp */}
+        <Graph data={weeklyData} metric={METRICS[0]} /> {/* 07 d temp */}
+        <Graph data={monthlyData} metric={METRICS[0]} /> {/* 01 M temp */}
+        <Graph data={dailyData} metric={METRICS[1]} /> {/* 24 h hum */}
+        <Graph data={weeklyData} metric={METRICS[1]} /> {/* 07 d hum */}
+        <Graph data={monthlyData} metric={METRICS[1]} /> {/* 01 M hum */}
+        <Graph data={dailyData} metric={METRICS[2]} /> {/* 24 h press */}
+        <Graph data={weeklyData} metric={METRICS[2]} /> {/* 07 d press*/}
+        <Graph data={monthlyData} metric={METRICS[2]} /> {/* 01 M press*/}
+        <MetricBox />
+        <MetricBox />
+      </div>
+    </>
+  )
+
 }
-
 
 function MetricBox() {
   return (
@@ -54,9 +62,9 @@ function MetricBox() {
 }
 
 export default function Board() {
-    return (
-        <>
-            <Tile />
-        </>
-    )
+  return (
+    <>
+      <Tile />
+    </>
+  )
 }
--- frontend/src/components/graph.tsx
+++ frontend/src/components/graph.tsx
@@ -1,56 +1,62 @@
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { type CurrentWeather, type WeatherHistory } from './../services/weatherService'
 import '../css/weather-chart.css'
+import { useMemo } from 'react';
 
-const data = [
-  { time: '00:00', temp: 12 },
-  { time: '04:00', temp: 10 },
-  { time: '08:00', temp: 15 },
-  { time: '12:00', temp: 22 },
-  { time: '16:00', temp: 24 },
-  { time: '20:00', temp: 18 },
-  { time: '23:59', temp: 14 },
-];
+export default function Graph({ data, metric }: { data: WeatherHistory | null, metric: any }) {
+  const formattedData = useMemo(() => {
+    if (!data) return [];
 
+    return data.map((entry) => {
+      const newPoint: any = {
+        time: entry.timestamp,
+      }
+      metric.keys.forEach((key: string) => {
+        newPoint[key] = entry[key as keyof CurrentWeather];
+      })
+      return newPoint;
+    })
+  }, [data, metric]);
 
-export default function Graph() {
-    return (
-        <div className="chart-container">
+  return (
+    <div className="chart-container">
       <h3 className="chart-title">temperature progression (24h)</h3>
       <div style={{ width: '100%', height: 300 }}>
         <ResponsiveContainer>
-          <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <AreaChart data={formattedData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
               <linearGradient id="colorTemp" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#acd9ff" stopOpacity={0.3}/>
-                <stop offset="95%" stopColor="#acd9ff" stopOpacity={0}/>
+                <stop offset="5%" stopColor="#acd9ff" stopOpacity={0.3} />
+                <stop offset="95%" stopColor="#acd9ff" stopOpacity={0} />
               </linearGradient>
             </defs>
             <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="rgba(255,255,255,0.1)" />
-            <XAxis 
-              dataKey="time" 
-              axisLine={false} 
-              tickLine={false} 
-              tick={{fill: 'white', opacity: 0.5, fontSize: 12}} 
+            <XAxis
+              dataKey="time"
+              axisLine={false}
+              tickLine={false}
+              tick={{ fill: 'white', opacity: 0.5, fontSize: 12 }}
             />
-            <YAxis 
-              hide={true} 
-              domain={['auto', 'auto']} 
+            <YAxis
+              hide={true}
+              domain={['auto', 'auto']}
             />
-            <Tooltip 
+            <Tooltip
               contentStyle={{ backgroundColor: 'rgba(0,0,0,0.6)', border: 'none', borderRadius: '10px', color: 'white' }}
               itemStyle={{ color: '#acd9ff' }}
             />
-            <Area 
-              type="monotone" 
-              dataKey="temp" 
-              stroke="#acd9ff" 
-              strokeWidth={3}
-              fillOpacity={1} 
-              fill="url(#colorTemp)" 
-            />
+            {metric.keys.map((keyName: string) => (
+              <Area
+                key={keyName}
+                type="monotone"
+                dataKey={keyName}
+                stroke="#acd9ff"
+                fill="url(#colorTemp)"
+              />
+            ))}
           </AreaChart>
         </ResponsiveContainer>
       </div>
     </div>
-    )
+  )
 }
